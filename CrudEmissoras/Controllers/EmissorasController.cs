ac50216e3becd28e51ad217d06bf0351ccdd95e7
Victor Couto <victor.vdt1@gmail.com>
2021-05-14 14:21:37 -0300

    feat: correcao verificacao resultado query

--- CrudEmissoras/Controllers/EmissorasController.cs
+++ CrudEmissoras/Controllers/EmissorasController.cs
@@ -101,16 +101,26 @@ namespace CrudEmissoras.Controllers
             var emissora = await _contexto.Emissoras
                 .FirstOrDefaultAsync(m => m.Id == id);
 
-            if (searchString)
+            var existeAudiencia =  _contexto.Audiencias.Where(s => s.Emissora_audiencia_id == emissora.Id).FirstOrDefault();
+            if(existeAudiencia != null) 
             {
-                var somatorioAudienciaPorDia = _contexto.Audiencias.Where(s => s.Emissora_audiencia_id == emissora.Id).Sum(s => s.Pontos_audiencia);
-                ViewBag.Message = "O somatório de pontos de audiencia para a Emissora " + emissora.Nome + " é " + somatorioAudienciaPorDia;
+                if (searchString)
+                {
+                    var somatorioAudienciaPorDia = _contexto.Audiencias.Where(s => s.Emissora_audiencia_id == emissora.Id).Sum(s => s.Pontos_audiencia);
+                    ViewBag.Message = "O somatório de pontos de audiencia para a Emissora " + emissora.Nome + " é " + somatorioAudienciaPorDia;
 
-            }else
+                }
+                else
+                {
+                    var mediaAudienciaPorDia = _contexto.Audiencias.Where(s => s.Emissora_audiencia_id == emissora.Id).Average(s => s.Pontos_audiencia);
+                    ViewBag.Message = "A média de pontos de audiencia para a Emissora " + emissora.Nome + " é " + mediaAudienciaPorDia;
+                }
+            }else 
             {
-                var mediaAudienciaPorDia = _contexto.Audiencias.Where(s => s.Emissora_audiencia_id == emissora.Id).Average(s => s.Pontos_audiencia);
-                ViewBag.Message = "A média de pontos de audiencia para a Emissora " + emissora.Nome + " é " + mediaAudienciaPorDia;
+                ViewBag.Message = "A Emissora "+ emissora.Nome + " nao possui Audiencias cadastradas !";
+
             }
+
 
             return View(emissora);
         }
